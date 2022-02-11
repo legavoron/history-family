6c5e867893f0ce9b00c6bc76e58cd64d1a7f7814
Aleh Shchurko <alehshchurko@gmail.com>
2022-02-11 11:01:26 +0300

    add new fotos

--- js/slide.js
+++ js/slide.js
@@ -27,12 +27,12 @@ function showSlides(classImage, next, prew, addres ,sumslides) {
     });
 }
 
-showSlides('.soya_img', '#next_soya_btn', '#prew_soya_btn', 'soya', 3);
-showSlides('.iva_img', '#next_iva_btn', '#prew_iva_btn', 'iva', 3);
+showSlides('.soya_img', '#next_soya_btn', '#prew_soya_btn', 'soya', 5);
+showSlides('.iva_img', '#next_iva_btn', '#prew_iva_btn', 'iva', 4);
 showSlides('.spain_img', '#next_spain_btn', '#prew_spain_btn', 'spain', 6);
-showSlides('.wedding_img', '#next_wedding_btn', '#prew_wedding_btn', 'wedding', 6);
-showSlides('.eu_img', '#next_eu_btn', '#prew_eu_btn', 'travel/europe', 3);
-showSlides('.ru_img', '#next_ru_btn', '#prew_ru_btn', 'travel/russia', 3);
-showSlides('.uk_img', '#next_uk_btn', '#prew_uk_btn', 'travel/ukraine', 3);
-showSlides('.ny_img', '#next_ny_btn', '#prew_ny_btn', 'ny', 5);
-showSlides('.last_img', '#next_last_btn', '#prew_last_btn', 'last', 22);
+showSlides('.wedding_img', '#next_wedding_btn', '#prew_wedding_btn', 'wedding', 14);
+showSlides('.eu_img', '#next_eu_btn', '#prew_eu_btn', 'travel/europe', 5);
+showSlides('.ru_img', '#next_ru_btn', '#prew_ru_btn', 'travel/russia', 5);
+showSlides('.uk_img', '#next_uk_btn', '#prew_uk_btn', 'travel/ukraine', 4);
+showSlides('.ny_img', '#next_ny_btn', '#prew_ny_btn', 'ny', 9);
+showSlides('.last_img', '#next_last_btn', '#prew_last_btn', 'last', 46);
